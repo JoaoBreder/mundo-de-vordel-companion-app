ef4714ea860810f72c0718825e867c2186f5fd3d
João Breder <joao@directon.com.br>
2025-07-13 02:38:09 -0300

    feat(angular-fire): Adicionado o angularfire ao projeto para implementação do firebase
     - Foi adicionado o angularfire ao projeto para conseguir implementar toda a lógica do firebase ao projeto
     - O arquivo de environment.ts foi adicionado ao projeto e ocultado pelo gitignore, para não vazar a configuração para a comunicação com o firebase, mas foi adicionado um arquivo de exemplo de como ele deve ser

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideFunctions, getFunctions } from '@angular/fire/functions';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [
+        provideFirebaseApp(() => initializeApp(environment.firebase)),
+        provideAuth(() => getAuth()),
+        provideFirestore(() => getFirestore()),
+        provideFunctions(() => getFunctions()),
+
+        BrowserModule,
+        AppRoutingModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
---
+++ frontend/src/environments/environtment.exemplo.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase: {
+        apiKey: null,
+        authDomain: null,
+        projectId: null,
+        storageBucket: null,
+        messagingSenderId: null,
+        appId: null,
+        measurementId: null,
+    },
+};
