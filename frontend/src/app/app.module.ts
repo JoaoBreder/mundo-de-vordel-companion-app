b1860da68e70237c67021b1c18fc1d38ad7d0786
JoaoBreder <joao.g.breder@gmail.com>
2025-07-14 13:41:14 -0300

    fix(toastr): Erro no Toastr
     - Não estava sendo colocado no módulo da aplicação o BrowserAnimationsModule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
@@ -20,6 +20,8 @@ import { AuthService } from './shared/services/auth.service';
         provideFunctions(() => getFunctions()),
 
         BrowserModule,
+        BrowserAnimationsModule,
+
         AppRoutingModule,
 
         ToastrModule.forRoot(),
