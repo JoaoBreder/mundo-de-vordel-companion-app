7626b4d895997a3cb57d06ee17e08b33074a9a4a
JoaoBreder <joao.g.breder@gmail.com>
2025-07-19 01:37:51 -0300

    feat(ficha-de-personagem): Implementado busca de informações do personagem jogador
     - Criado FichaDePersonagemService para tratar de métodos mais complexos e comunicação com funções do back
     - Foi implementado no FichaDePersonagemService o resolve e canDeactivate, para que ele pudesse ser declarado nesses campos no FichaDePersonagemRoutingModule, fazendo com que o método de busca dos dados do personagem seja chamado na construção da tela do componente
     - Criado método observarPersonagemJogador com implementação provisória da visualização das informações da ficha do personagem

--- frontend/src/app/pages/ficha-de-personagem/ficha-de-personagem-routing.module.ts
+++ frontend/src/app/pages/ficha-de-personagem/ficha-de-personagem-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FichaDePersonagemComponent } from './component/ficha-de-personagem.component';
+import { FichaDePersonagemService } from './ficha-de-personagem.service';
 
 const routes: Routes = [
     {
         path: '',
         component: FichaDePersonagemComponent,
+        resolve: {
+            data: FichaDePersonagemService,
+        },
+        canDeactivate: [FichaDePersonagemService],
     },
 ];
 
